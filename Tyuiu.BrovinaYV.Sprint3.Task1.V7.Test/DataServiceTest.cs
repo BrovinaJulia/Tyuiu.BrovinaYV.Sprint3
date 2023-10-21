16b78725e27934a22b41187b6a9928d643769c37
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-10-21 16:35:06 +0500

    Task1

---
+++ Tyuiu.BrovinaYV.Sprint3.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BrovinaYV.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.BrovinaYV.Sprint3.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValidMultiply ()
+        {
+            DataService ds = new DataService();
+            double wait = -0.930;
+            double res = ds.GetMultiplySeries(0.25, 1, 9);
+
+            Assert.AreEqual(wait, res, 9);
+        }
+    }
+}
---
+++ Tyuiu.BrovinaYV.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BrovinaYV.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.BrovinaYV.Sprint3.Task1.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Бровина Ю. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант 7                                                               *");
+            Console.WriteLine("* Выполнил: Бровина Юлия Викторовна | АСОиУб-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет              *");
+            Console.WriteLine("* произведение ряда по формуле, при a=0,25                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+
+            Console.WriteLine("Переменная A = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
